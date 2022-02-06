572ee6df72ea6b360d22313b2c43a8e4017af019
pakpanyi <pakpanyi@gmail.com>
2022-02-06 16:26:45 -0400

    added status

--- src/pages/NavPages/Home.js
+++ src/pages/NavPages/Home.js
@@ -5,6 +5,7 @@ import ProfilePic from "../../images/4Github.png";
 import Translation from "../../utils/Translation.json";
 import { AppContext } from "../../utils/AppContext";
 import { BiLinkExternal } from "react-icons/bi";
+import { BsStarFill } from "react-icons/bs";
 import Typed from "react-typed";
 
 export default function HomePage() {
@@ -12,15 +13,23 @@ export default function HomePage() {
   return (
     <div className="home-page-container">
       <div className="home-page-div">
-        <img src={ProfilePic} className="home-page-img" alt="profile" />
+        <div className="home-page-inner-div align-center">
+          <img src={ProfilePic} className="home-page-img" alt="profile" />
+        </div>
+        <div className="home-page-status">
+          <BsStarFill />
+          <p className="status-title">Status :</p>
+          <p className="status-description">{Translation[74][language]}</p>
+        </div>
       </div>
       <div className="home-page-div">
-        <h3 className="home-page-h3">
-          {"<HelloWorld, "}
-          {Translation[6][language]}
-          {"/>"}
-        </h3>
-        {/* <div className="home-spinner-div">
+        <div className="home-page-inner-div">
+          <h3 className="home-page-h3">
+            {"<HelloWorld, "}
+            {Translation[6][language]}
+            {"/>"}
+          </h3>
+          {/* <div className="home-spinner-div">
           <BarLoader
             height={4}
             width={200}
@@ -28,20 +37,21 @@ export default function HomePage() {
             speedMultiplier={0.75}
           />
         </div> */}
-        <h1 className="home-page-h1">Philip Akpanyi</h1>
-        <div className="frontend-dev-container">
-          <hr className="horizontal-line" />
-          <div className="frontend-box-container">
-            <div className="frontend-text-container">
-              {/* <h5 className="frontend-text-h5">Front-end Dev</h5> */}
-              <Typed
-                strings={["Front-end Dev", "Backend Dev"]}
-                className="frontend-text-h5"
-                typeSpeed={120}
-                backSpeed={120}
-                backDelay={1000}
-                startDelay={1000}
-                loop></Typed>
+          <h1 className="home-page-h1">Philip Akpanyi</h1>
+          <div className="frontend-dev-container">
+            <hr className="horizontal-line" />
+            <div className="frontend-box-container">
+              <div className="frontend-text-container">
+                {/* <h5 className="frontend-text-h5">Front-end Dev</h5> */}
+                <Typed
+                  strings={["Front-end Dev", "Backend Dev"]}
+                  className="frontend-text-h5"
+                  typeSpeed={120}
+                  backSpeed={120}
+                  backDelay={1000}
+                  startDelay={1000}
+                  loop></Typed>
+              </div>
             </div>
           </div>
         </div>
